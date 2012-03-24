0eb66d43c8c7b44d47fc520548f7e8d3916fcf5f
unknown <Home@Home-PC.(none)>
2012-03-24 02:17:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philowikicounter::Application.config.secret_token = 'aef940ac84be192aa4c07a2f33e2762191cff8fd23eee17c93249dd9c51a692bb86e465b656439c4da4ea61d7aab9a8064cf028911777e61b24f9c541495b451'
